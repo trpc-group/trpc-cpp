bacbc0678d61a0421355e2ea5698c985e92d7d58
chhy2009 <chenhuiyong2009@gmail.com>
2024-01-30 19:13:49 +0800

    BugFix: fix the problem of uneven traffic using backup request in IP direct connection scenario (#106)

--- trpc/naming/common/util/utils_help.cc
+++ trpc/naming/common/util/utils_help.cc
@@ -15,24 +15,44 @@
 
 #include <stdlib.h>
 
+#include "trpc/util/algorithm/random.h"
+
 namespace trpc {
 
 int SelectMultiple(const std::vector<TrpcEndpointInfo>& src, std::vector<TrpcEndpointInfo>* dst,
                    const int select_size) {
+  if (src.empty()) {
+    return -1;
+  }
+
   int need_select_size = select_size;
   int instances_size = src.size();
   if (need_select_size > instances_size) {
     need_select_size = instances_size;
   }
-  int pos = rand() % instances_size;
-  if (pos + need_select_size > instances_size) {
-    // Add instances_size-pos elements
-    dst->assign(src.begin() + pos, src.end());
-    // Add need_select_size - (instances_size-pos) elements
-    int reserve_size = need_select_size - (instances_size - pos);
-    dst->insert(dst->end(), src.begin(), src.begin() + reserve_size);
-  } else {
-    dst->assign(src.begin() + pos, src.begin() + pos + need_select_size);
+  dst->reserve(need_select_size);
+
+  int pos = trpc::Random<uint32_t>() % instances_size;
+  dst->push_back(src[pos]);
+  if (instances_size == 1) {
+    return 0;
+  }
+
+  // Randomly obtain the offset of the next node to be retrieved and retrieve 'need_select_size - 1' nodes
+  // continuously (skipping the selected node).
+  int pos_offset = trpc::Random<uint32_t>() % (instances_size - 1) + 1;
+  int next_pos = pos + pos_offset;
+  for (int i = 1; i < need_select_size; i++) {
+    if (next_pos >= instances_size) {
+      next_pos -= instances_size;
+    }
+    if (next_pos == pos) {
+      next_pos++;
+    }
+    if (next_pos >= instances_size) {
+      next_pos -= instances_size;
+    }
+    dst->push_back(src[next_pos++]);
   }
 
   return 0;
