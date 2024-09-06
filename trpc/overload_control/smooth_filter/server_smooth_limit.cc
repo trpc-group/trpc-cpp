54c9b69080388583c8e9097bb239d7bac33b55df
2549141519 <2549141519@qq.com>
2024-09-06 19:56:10 +0800

    改用flow_control的smooth_limiter

--- trpc/overload_control/smooth_filter/server_smooth_limit.cc
+++ trpc/overload_control/smooth_filter/server_smooth_limit.cc
@@ -24,75 +24,30 @@
 namespace trpc::overload_control {
 
 bool SmoothLimit::Init() {
-    return tick_timer_.Init();
+  // nothing to do,The time thread automatically starts.
+  return true;
 }
 
 bool SmoothLimit::BeforeSchedule(const ServerContextPtr& context) {
-   
-    return CheckLimit(context);
+    return smooth_limit_.CheckLimit(context);
 }
 
-
-
 void SmoothLimit::Destroy(){
-    tick_timer_.Destroy();
+    // nothing to do,The time thread automatically destorys.
 }
 
 void SmoothLimit::Stop(){
-    tick_timer_.Stop();
+    // nothing to do,The time thread automatically stops.
 };
-
-
 
 SmoothLimit::SmoothLimit(std::string name,int64_t limit, bool is_report, int32_t window_size)
     : name_(name),
-      limit_(limit),
-      is_report_(is_report),
-      window_size_(window_size <= 0 ? kDefaultNum : window_size),
-      requestrollque_(window_size_),
-      tick_timer_(std::chrono::microseconds(static_cast<int64_t>(std::ceil(1000000.0 / window_size_))),
-                  [this] { OnNextTimeFrame(); }) {
-  TRPC_ASSERT(window_size_ > 0);
+      smooth_limit_(limit, is_report, window_size) {
 }
 
 //The destructor does nothing, but the stop method in public needs to set the timed task to join and make it invalid
 //The user must stop before calling destroy
 SmoothLimit::~SmoothLimit() {}
-
-//Check if the current limit is exceeded
-bool SmoothLimit::CheckLimit(const ServerContextPtr& context) {
-  bool ret = false;
-  int64_t active_sum = requestrollque_.ActiveSum();
-  
-  int64_t hit_num = 0;
-  if (active_sum >= limit_) {
-    ret = true;
-  } else {
-    hit_num = requestrollque_.AddTimeHit();
-    
-    
-    if (hit_num > limit_) {
-      ret = true;
-    }
-  }
-  if (is_report_) {
-    OverloadInfo infos;
-    infos.attr_name = "SmoothLimiter";
-    infos.report_name = context->GetFuncName();
-    infos.tags["active_sum"] = active_sum;
-    infos.tags["hit_num"] = hit_num;
-    infos.tags["max_qps"] = limit_;
-    infos.tags["window_size"] = window_size_;
-    infos.tags[kOverloadctrlPass] = (ret ? 0 : 1);
-    infos.tags[kOverloadctrlLimited] = (ret ? 1 : 0);
-    Report::GetInstance()->ReportOverloadInfo(infos);
-  }
-  return ret;
-}
-
-int64_t SmoothLimit::GetCurrCounter() { return requestrollque_.ActiveSum(); }
-
-void SmoothLimit::OnNextTimeFrame() { requestrollque_.NextTimeFrame(); }
 
 }  // namespace trpc::overload_control
 #endif
--- trpc/overload_control/smooth_filter/server_smooth_limit.h
+++ trpc/overload_control/smooth_filter/server_smooth_limit.h
@@ -14,46 +14,25 @@
 #ifdef TRPC_BUILD_INCLUDE_OVERLOAD_CONTROL
 #pragma once
 
-#include "trpc/overload_control/smooth_filter/server_request_roll_queue.h"
-#include "trpc/overload_control/smooth_filter/server_trick_time.h"
 #include "trpc/overload_control/smooth_filter/server_overload_controller.h"
+#include "trpc/overload_control/flow_control/smooth_limiter.h"
 
 #include<string>
 #include <cstdint>
 
 #include "trpc/util/function.h"
 
-/// @brief The first step is to implement a time queue that stores the number of requests
-/// @note We use a queue to represent a time window of 1s, and the number of queues N represents N time slots
-/// Optimization 1: We suggest that the size of N be 2 ^ n-2, so that frequent redundancy operations can be replaced by AND operations. 
-/// We have considered directly expanding the user's memory to 2 ^ n-2, but in reality, the expansion may take up a large amount of memory, so this is just a suggestion
-/// Optimization 2: We add an atomic variable to represent the total number of smooth windows, which can save the need to calculate for loops every time (which involves concurrency control)
-/// The queue itself uses atomic to represent some intra class elements, ensuring that the atomicity of individual variable modification operations is not strictly synchronized
-/// But when calling the checkpoint() of this queue, a double loop is used to ensure that there will not be a moment when the threshold is exceeded
-
-/// The second step of the plugin requires a timer to process the forward movement of the window at a scheduled time
-/// The timer here needs to open a thread to do so, so it requires interfaces for creating, starting, and destroying threads
-
-/// 1. Implement a circular queue using vector<uint64_t>of size N
-/// 2. The size of the circular queue represents the number of time slots divided into 1s
-/// 3. Each time slot corresponds to an integer element in the queue, storing the cumulative number of requests in that time slot
-/// 4. The passage of time corresponds to the movement of the 'smooth window'
-/// Every time the timer is triggered (once every 1/N second), the oldest time slot is removed and a new time slot is added (the count is reset to 0)
-/// 5. The number of requests in the current time slot accumulates to the count of the latest time slot
-/// 6. Accumulate the count within the current queue range when calculating the total number of requests within 1s
-/// 7. The rotation of the time slot is achieved by calling the NextFrame() function through an external timer thread
-
 namespace trpc::overload_control 
 {
-/// @var: Default number of time frames per second
-constexpr int32_t kDefaultNum = 100;
+/// @brief Default number of time frames per second
+static const int32_t kDefaultNumber = 100;
 
 class SmoothLimit : public ServerOverloadController
 {
 public:
   /// @brief Initialize the sliding window current limiting plugin
   /// @param Name of plugin Current limit quantity Record monitoring logs or not Number of time slots
-  explicit SmoothLimit(std::string name,int64_t limit, bool is_report = false, int32_t window_size = kDefaultNum);
+  explicit SmoothLimit(std::string name,int64_t limit, bool is_report = false, int32_t window_size = kDefaultNumber);
 
   /// @note Do nothing, the entire plugin requires manual destruction of only thread resources
   /// @brief From the implementation of filter, it can be seen that before destruction
@@ -78,37 +57,10 @@ public:
 
   /// @brief Destroy thread
   void Destroy() ;
-public: //This place should be private. I tested it in the test, so it was defined as public
-  ///@brief Check if it exceeds the current limit of the window
-  bool CheckLimit(const ServerContextPtr& check_param);
-
-  /// @brief Retrieve the number of requests occurring within the current window
-  int64_t GetCurrCounter() ;
-
-  /// @brief Obtain the current limiting threshold
-  int64_t GetMaxCounter() const  { return limit_; }
-public:
-  //Name of plugin
+private:
   std::string name_;
 
-  //Regularly call this func to move the smooth window
-  void OnNextTimeFrame();
-
-  //Number of requests limited by smooth window
-  int64_t limit_;
-
-  //Whether to record monitoring data
-  bool is_report_{false};
-
-  //The number of time slots in the smooth window
-  int32_t window_size_;
-
-  //Store the smooth window time slot for requests
-  RequestRollQueue requestrollque_;
-
-  //Timer starts, thread calls NextFrame() every 1/N second to move the smooth window
-  TickTime tick_timer_;
-
+  SmoothLimiter smooth_limit_;
 };
 
 }
