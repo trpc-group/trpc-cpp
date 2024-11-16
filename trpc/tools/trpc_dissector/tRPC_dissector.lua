bc9a5c06573e3a285d42520040fba430b21925c1
Chen_zhuozhuo <1014710855@qq.com>
2024-11-16 14:21:00 +0800

    Dynamic identify service port, correct format and variable names

--- trpc/tools/trpc_dissector/tRPC_dissector.lua
+++ trpc/tools/trpc_dissector/tRPC_dissector.lua
@@ -4,7 +4,7 @@ do
     
     local field_magic      = ProtoField.uint16(protocol_name .. ".magic",       "Magic",       base.HEX)
     local field_type       = ProtoField.uint8(protocol_name .. ".type",         "Packet Type", base.DEC)
-    local field_stream     = ProtoField.uint8(protocol_name .. ".stream",       "Stream",      base.DEC)
+    local field_stream     = ProtoField.uint8(protocol_name .. ".stream",       "Stream Type", base.DEC)
     local field_total_size = ProtoField.uint32(protocol_name .. ".total_size",  "Total Size",  base.DEC)
     local field_header_size= ProtoField.uint16(protocol_name .. ".header_size", "Header Size", base.DEC)
     local field_unique_id  = ProtoField.uint32(protocol_name .. ".unique_id",   "Unique ID",   base.DEC)
@@ -12,23 +12,39 @@ do
     local field_reserved   = ProtoField.uint8(protocol_name .. ".reserved",     "Reserved",    base.DEC)
     trpc_proto.fields      = {field_magic, field_type, field_stream, field_total_size, field_header_size, field_unique_id, field_version, field_reserved}
 	
-    local MAGIC_CODE_PRPC = "0930"
+    local MAGIC_CODE_TRPC = "0930"
     local PROTO_HEADER_LENGTH = 16
 
-    local server_port      = 12345
     local tcp_src_port     = Field.new("tcp.srcport")
     local tcp_dst_port     = Field.new("tcp.dstport")
+    local tcp_stream       = Field.new("tcp.stream")
 
     local data_dissector     = Dissector.get("data")
     local protobuf_dissector = Dissector.get("protobuf")
 
-    function trpc_proto.dissector(buffer, packet, tree) 
+    function trpc_proto.init()
+        -- tcp_stream_id <-> {client_port, server_port}
+        stream_map = {}
+    end
+
+    function trpc_proto.dissector(buffer, packet, tree)
         packet.cols.protocol:set("tRPC")
 
-        if tcp_src_port()() == server_port then
-            packet.private["pb_msg_type"] = "message,trpc.ResponseProtocol"
-        elseif tcp_dst_port()() == server_port then
+        local f_src_port = tcp_src_port()()
+        local f_dst_port = tcp_dst_port()()
+
+        -- record which one is client and which one is server
+        -- by first tcp syn frame
+        stream_n = tcp_stream().value
+        if stream_map[stream_n] == nil then
+            stream_map[stream_n] = {f_src_port, f_dst_port}
+        end
+
+        if f_src_port == stream_map[stream_n][1] then
             packet.private["pb_msg_type"] = "message,trpc.RequestProtocol"
+        end
+        if f_src_port == stream_map[stream_n][2] then
+            packet.private["pb_msg_type"] = "message,trpc.ResponseProtocol"
         end
 
         local magic_value       = buffer(0, 2)
@@ -55,7 +71,7 @@ do
         t:add(field_magic, magic_value)
         t:add(field_type, type_value)
         t:add(field_stream, stream_value)
-        t:add(field_total_size, total_size_value) 
+        t:add(field_total_size, total_size_value)
         t:add(field_header_size, header_size_value)
         t:add(field_unique_id, unique_id_value)
         t:add(field_version, version_value)
@@ -66,14 +82,13 @@ do
 
     -- heuristic
     local function heur_dissect_proto(tvbuf, pktinfo, root)
-
         if (tvbuf:len() < PROTO_HEADER_LENGTH) then
             return false
         end
 	
 	    local magic = tvbuf:range(0, 2):bytes():tohex()
-      -- for range dissectors
-        if magic ~= MAGIC_CODE_TRPC  then
+        -- for range dissectors
+        if magic ~= MAGIC_CODE_TRPC then
             return false
         end
         trpc_proto.dissector(tvbuf, pktinfo, root)
